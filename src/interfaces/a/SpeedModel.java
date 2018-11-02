181a97c00ba63d1affb2f5592e30324cd8b79baf
lihui <lihui@RI-PC.jp.oracle.com>
2018-11-02 19:17:08 +0900

    interface

---
+++ src/interfaces/a/SpeedModel.java
@@ -0,0 +1,5 @@
+package interfaces.a;
+
+public interface SpeedModel {
+	double getSpeedMph(double timeSec, int weightPounds, int horsePower);
+}
---
+++ src/interfaces/a/Vehicle.java
@@ -0,0 +1,6 @@
+package interfaces.a;
+
+public interface Vehicle {
+	void setSpeedModel(SpeedModel speedModel);
+    double getSpeedMph(double timeSec);
+}
