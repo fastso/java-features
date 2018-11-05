d5529b675f99a7fd498bb64ed928eb4c8ec99c41
lihui <fastso.biko@gmail.com>
2018-11-05 15:50:36 +0900

    interface

--- src/interfaces/a/Truck.java
+++ src/interfaces/a/Truck.java
@@ -3,6 +3,14 @@ package interfaces.a;
 public interface Truck extends Vehicle {
 	int getPayloadPounds();
 
+	/**
+	 * Default Method of Interface<br>
+	 * If the class implements it, the interface's default implementation is ignored
+	 * and overridden by the class implementation.<br>
+	 * Since Java 8<br>
+	 * 
+	 * @return
+	 */
 	default int getPayloadKg() {
 		return convertPoundsToKg(getPayloadPounds());
 	}
@@ -12,8 +20,10 @@ public interface Truck extends Vehicle {
 	}
 
 	/**
+	 * Private Method of Interface<br>
 	 * A private interface method not used by other methods of the same interface
-	 * does not make sense. (Since Java 9)
+	 * does not make sense.<br>
+	 * Since Java 9<br>
 	 * 
 	 * @param pounds
 	 * @return
@@ -22,6 +32,15 @@ public interface Truck extends Vehicle {
 		return (int) Math.round(0.454 * pounds);
 	}
 
+	/**
+	 * Static Method of Interface<br>
+	 * A static method of an interface cannot be overridden by the class that
+	 * implements this interface<br>
+	 * Since Java 8<br>
+	 * 
+	 * @param kilograms
+	 * @return
+	 */
 	static int convertKgToPounds(int kilograms) {
 		return (int) Math.round(2.205 * kilograms);
 	}
