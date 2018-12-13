9c779cbc7dc37c16afde58be991611458cf64f36
lihui <fastso.biko@gmail.com>
2018-12-13 11:46:05 +0900

    effective java

---
+++ src/effectivejava3rd/generics/item32/Combine generics and varargs judiciously.md
@@ -0,0 +1,5 @@
+* It is unsafe to store a value in a generic varargs array parameter.
+* The SafeVarargs annotation constitutes a promise by the author of a method that it is typesafe.
+* It is unsafe to give another method access to a generic varargs parameter array.
+* Use @SafeVarargs on every method with a varargs parameter of a generic or parameterized type.
+
---
+++ src/effectivejava3rd/generics/item32/PickTwo.java
@@ -0,0 +1,29 @@
+package effectivejava3rd.generics.item32;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+//Subtle heap pollution
+public class PickTwo {
+	// UNSAFE - Exposes a reference to its generic parameter array!
+	static <T> T[] toArray(T... args) {
+		return args;
+	}
+
+	static <T> T[] pickTwo(T a, T b, T c) {
+		switch (ThreadLocalRandom.current().nextInt(3)) {
+		case 0:
+			return toArray(a, b);
+		case 1:
+			return toArray(a, c);
+		case 2:
+			return toArray(b, c);
+		}
+		throw new AssertionError(); // Can't get here
+	}
+
+	public static void main(String[] args) {
+		String[] attributes = pickTwo("Good", "Fast", "Cheap");
+		System.out.println(Arrays.toString(attributes));
+	}
+}
