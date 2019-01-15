1e42618ee517d4a02f05e3544b9771e5e4cebab3
lihui <fastso.biko@gmail.com>
2019-01-15 11:53:20 +0900

    effective java

---
+++ src/effectivejava3rd/lambdasandstreams/item43/Freq.java
@@ -0,0 +1,21 @@
+package effectivejava3rd.lambdasandstreams.item43;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+// Frequency table implemented with map.merge, using lambda and method reference
+public class Freq {
+	public static void main(String[] args) {
+		Map<String, Integer> frequencyTable = new TreeMap<>();
+
+		for (String s : args)
+			frequencyTable.merge(s, 1, (count, incr) -> count + incr); // Lambda
+		System.out.println(frequencyTable);
+
+		frequencyTable.clear();
+		for (String s : args)
+			frequencyTable.merge(s, 1, Integer::sum); // Method reference
+		System.out.println(frequencyTable);
+
+	}
+}
